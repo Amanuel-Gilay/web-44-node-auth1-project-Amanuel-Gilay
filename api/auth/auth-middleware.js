77a5ca917db15638bf6e21706d6744ac9ec10e19
Amanuel-Gilay <emahawi27@gmail.com>
2021-08-16 23:21:23 -0700

    wip

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -63,11 +63,12 @@ async function checkUsernameExists(req,res,next) {
   }
 */
 function checkPasswordLength(req,res,next) {
-  if(req.session.user) {
-    next()
-  }else {
-    next({ status: 422, message: 'Password must be longer than 3 chars'})
-  }
+if(!req.body.password || req.body.password.length <3 ){
+  next({ message:'password must be longer then 3 chars', status: 422})
+
+}else {
+  next()
+}
 
 }
 
